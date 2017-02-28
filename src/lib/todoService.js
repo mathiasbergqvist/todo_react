d2e576960fc6129587c66d58323c55a587da22f6
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2017-02-28 20:45:08 +0100

    Save data to server with fetch

--- src/lib/todoService.js
+++ src/lib/todoService.js
@@ -1,6 +1,18 @@
-const baseUrls = 'http://localhost:8080/todos';
+const baseUrl = 'http://localhost:8080/todos';
 
 export const loadTodos = () => {
-  return fetch(baseUrls)
+  return fetch(baseUrl)
     .then(res => res.json())
+}
+
+export const createTodo = (todo) => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(todo)
+  })
+  .then(res => res.json());
 }
